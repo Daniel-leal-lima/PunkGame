95ee1104c28febc1b5a54126b98a71241ac57f87
Daniel-leal-lima <daniel.leal.lima@hotmail.com>
2024-08-24 22:43:06 -0300

    move fix

--- Assets/Scripts/Player/Move.cs
+++ Assets/Scripts/Player/Move.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     [SerializeField] float speed = 5;
-   
+
+    Vector3 moveInput;
+    float moveHorizontal;
+    float moveVertical;
+
     Rigidbody _rb;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
     }
+    private void Update()
+    {
+        moveInput = new Vector3(moveHorizontal, 0, moveVertical);
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+    }
     void FixedUpdate()
     {
-        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        Vector3 moveDirection = moveInput.normalized * speed * Time.fixedDeltaTime;
-        _rb.MovePosition(_rb.position + moveDirection);
+        if (moveInput.magnitude > 0)
+        {
+            Vector3 movement = transform.right * moveHorizontal + transform.forward * moveVertical;
+
+            _rb.MovePosition(_rb.position + movement * speed * Time.deltaTime);
+        }
     }
 }
